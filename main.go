44fd23e9f5454c827108be80711b1ffe21799b7c
Anthony Heitzeberg <a.heitzeberg007@gmail.com>
2024-08-31 19:58:00 -0500

    build client listener

--- main.go
+++ main.go
@@ -2,8 +2,45 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Listening on port :6379")
+
+	// Create new server
+	l, err := net.Listen("tcp", ":6379")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Start listening for connections
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer conn.Close() // close connection once finished
+
+	// create loop to receive commands from client and respond to them
+	for {
+		buf := make([]byte, 1024)
+
+		// read messages from client
+		_, err := conn.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("error reading from client: ", err.Error())
+			os.Exit(1)
+		}
+
+		// ignore request and send back a PONG
+		conn.Write([]byte("+OK\r\n"))
+	}
 }
